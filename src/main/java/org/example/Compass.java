4fc48f82cb42029fd69af8490dd4d3a94b3cc850
Joel <joel@MacBook-Pro-Alex.local>
2024-07-29 11:49:23 +0100

    Test: rotate test passes for all points - Direction Right

--- src/main/java/org/example/Compass.java
+++ src/main/java/org/example/Compass.java
@@ -13,6 +13,17 @@ public class Compass {
     }
     String point = String.valueOf(Point.NORTH);
     public String rotate(String point, String direction) {
+
+        if (point.equalsIgnoreCase(Point.NORTH.toString()) && direction.equalsIgnoreCase(Direction.RIGHT.toString())) {
+            return Point.EAST.toString();
+        } else if (point.equalsIgnoreCase(Point.EAST.toString()) && direction.equalsIgnoreCase(Direction.RIGHT.toString())) {
+            return Point.SOUTH.toString();
+        } else if (point.equalsIgnoreCase(Point.SOUTH.toString()) && direction.equalsIgnoreCase(Direction.RIGHT.toString())) {
+            return Point.WEST.toString();
+        } else if (point.equalsIgnoreCase(Point.WEST.toString()) && direction.equalsIgnoreCase(Direction.RIGHT.toString())) {
+            return Point.NORTH.toString();
+
+        }
         return null;
     }
 }
--- src/test/java/org/example/CompassTest.java
+++ src/test/java/org/example/CompassTest.java
@@ -8,20 +8,25 @@ import static org.junit.jupiter.api.Assertions.*;
 class CompassTest {
 
     @Test
-    @DisplayName("return Point EAST when passed a NORTH-facing Point and Direction RIGHT.")
+    @DisplayName("return next Point when passed a previous-facing Point and Direction RIGHT.")
     void rotate() {
         //Arrange
         Compass compass = new Compass();
-        String point = Compass.Point.NORTH.toString();
+//        String point = Compass.Point.NORTH.toString();
         String direction = Compass.Direction.RIGHT.toString();
-        String expectedResult = Compass.Point.EAST.toString();
+//        String expectedResult = Compass.Point.EAST.toString();
 
         //Act
-        String result = compass.rotate(point, direction);
-
+//        String result = compass.rotate(point, direction);
 
         //Assert
-        assertEquals(expectedResult, result);
+//        assertEquals(expectedResult, result);
+        assertAll("Testing Direction Right for all Points",
+            () -> assertEquals(Compass.Point.EAST.toString(), compass.rotate(Compass.Point.NORTH.toString(), direction)),
+            () -> assertEquals(Compass.Point.SOUTH.toString(), compass.rotate(Compass.Point.EAST.toString(), direction)),
+            () -> assertEquals(Compass.Point.WEST.toString(), compass.rotate(Compass.Point.SOUTH.toString(), direction)),
+            () -> assertEquals(Compass.Point.NORTH.toString(), compass.rotate(Compass.Point.WEST.toString(), direction))
+        );
 
     }
 }
