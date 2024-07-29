549b8db612b06cf5dcf1903d603769793af88883
andrei-vasiliu-coding <andrei-vasiliu@outlook.com>
2024-07-29 11:09:56 +0100

    feat: enum point and rotation

---
+++ src/main/java/org/example/Compass.java
@@ -0,0 +1,18 @@
+package org.example;
+
+public class Compass {
+    public enum Point {
+        NORTH,
+        SOUTH,
+        WEST,
+        EAST
+    }
+    public enum Direction {
+        LEFT,
+        RIGHT
+    }
+    String point = String.valueOf(Point.NORTH);
+    public String rotate(String point, String direction) {
+        return null;
+    }
+}
